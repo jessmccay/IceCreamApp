dc27f9dea6dbb0e0f43f87b26bef948e7832a5f6
jessmccay <jessmccay@gmail.com>
2017-08-27 11:05:00 -0700

    console app, main method and class ice cream

---
+++ IceCreamApp/IceCream.cs
@@ -0,0 +1,16 @@
+﻿namespace IceCreamApp
+{
+    public class IceCream
+    {
+        private string _color;
+        public string Color
+        {
+            get { return Color; }
+            set { Color = _color; }
+        }
+        public IceCream(string color)
+        {
+            _color = color;
+        }
+    }
+}
---
+++ IceCreamApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the ice cream app!");
+            Console.WriteLine("Would you like to make an ice cream?");
+            string answer = Console.ReadLine();
+
+            if (answer.ToLower() == "yes")
+            {
+                Console.Write("What flavor of ice cream would you like?");
+                string userColor = Console.ReadLine();
+                IceCream usersIceCream = new IceCream(userColor);
+                Console.WriteLine($"You created a {userColor} ice cream!");
+                Console.Read();
+            }
+           
+        }
+    }
+}
